87bfe2f5f88b791ea56644cf1b8b68121c6f9d70
pippo1404 <46838818+pippo1404@users.noreply.github.com>
2019-01-19 22:05:07 +0800

    Add Pipo's Project

---
+++ Pipo's Unity Project/Assets/CubeMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+//Ctrl + S to save
+//See console in unity to debug or see any problem
+
+public class CubeMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
+
+    // Update is called once per frame
+   
