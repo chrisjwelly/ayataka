cb1b8782d9d342a0ffe1aa23362111bf3a4b7e76
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2019-01-20 07:10:14 +0800

    Add a distance-based trigger for obstacle to start shooting

---
+++ New Unity Project/Assets/Scripts/Shooting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// plan:
+/*
+ * 1. Create prefab for bullet (Done!)
+ * 2. Instantiate the bullet [from the obstacle]
+ * 3. If (get key smth smth) -> Shoot the bullet [by addForce?]
+ * 4. 
+ */
+public class Shooting : MonoBehaviour
+{
+    public Rigidbody bullet;
+    public Transform obstacleInfo;
+    public Transform playerInfo;
+    public int distanceBetween = 50; // distance between the obstacle and the player must be lower than this value
+    public float bulletForce = 3000f;
+
+    void FixedUpdate()
+    {
+        if (obstacleInfo.position.z - playerInfo.position.z < distanceBetween)
+        {
+            /* don't use GetKey because this is for all the duration
+             * that the button is pressed down. GetKeyDown will
+             * only return true for the single instant the button is pressed
+             * down
+             */
+            if (Input.GetKeyDown("k"))
+            {
+                Rigidbody bulletInstance;
+                bulletInstance = Instantiate(bullet, obstacleInfo.position, obstacleInfo.rotation) as Rigidbody;
+                bulletInstance.AddForce(0, 0, -bulletForce);
+                // I LOVE IGNORE COLLISION
+                Physics.IgnoreCollision(bulletInstance.GetComponent<Collider>(), GetComponent<Collider>());
+            }
+        }
+        Debug.Log(obstacleInfo.position.z - playerInfo.position.z); // add a string here to help you maybe
+    }
+}
