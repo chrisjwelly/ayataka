2ee288c61242e2949dc556c0de18194e9c4f9545
heze8 <46834785+heze8@users.noreply.github.com>
2019-01-20 09:44:40 +0800

    Add models

---
+++ New Unity Project/Generator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject[] obj;
+    public Rigidbody RB2;
+
+
+    void Start()
+    {
+        spawn();
+    }
+
+    public float z = 45f;
+    public float g_pos = 490f;
+    public Transform ground;
+    void FixedUpdate()
+    {
+        float z_pos = RB2.position.z;
+        if (z_pos > z)
+        {
+            z = z + 160f;
+            spawn();
+
+        }
+        if (z_pos > g_pos)
+        {
+            g_pos = g_pos + 1000f;
+            Instantiate(ground, new Vector3(0, 0, g_pos), Quaternion.identity);
+        }
+    }
+    void spawn()
+    {
+        for (int y = 0; y < 4; y++)
+        {
+
+            Instantiate(obj[Random.Range(0, obj.GetLength(0))], new Vector3(0, 1, z + y * 40), Quaternion.identity);
+
+        }
+    }
+}
